9460fddeb97348d9f5000b0a90a270b989a52405
vijsak27 <146001383+vijsak27@users.noreply.github.com>
2025-11-23 04:22:22 +0000

    Next Steps
    1. Fix the addManually
    2. check if the -1 values in tables[][] are correct

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@
 public class Main{
     public static void main (String[] args){
         Conference c1 = new Conference(10,10);
+        c1.addManually();
         c1.emptyFill();
         c1.readFile();
         c1.organize();
