e12c900f188a51bf843a3f89bbb90df74c8f3a9e
Vij <vijsak27@7hills.org>
2025-11-25 09:30:37 -0500

    added UI clarity -- need to finish the menu

--- Main.java
+++ Main.java
@@ -12,21 +12,23 @@ public class Main{
         c1.emptyFill();//fill tables empty attendees
         c1.readFile();// read in the file
         boolean addMore = true;//starts off as true so thaty user is prompted to add attendees
-        do{
-            System.out.println("Would you like to add any attendees? (y/n)");
-            if((s.nextLine().equals("y"))){
-                boolean added = c1.addManually();//run add manually if the user wants to add an attendee. save whether it was added to a boolean
+        System.out.println("Menu:\n1. Organize seats\n2.Add Attendee Manually");
+        
+			do{
+				System.out.println("Would you like to add any attendees? (y/n)");
+				if((s.nextLine().equals("y"))){
+					boolean added = c1.addManually();//run add manually if the user wants to add an attendee. save whether it was added to a boolean
 
-                if (!added){//if not added
-                    addMore=false;// stop to loop by making addMore = false because mas occupancy has been reached
-                }
-                addMore = true;// else addMore = true so keep going
-                
-            }
-            else{
-                addMore=false;// if users enteres anything other than "y" stop the loop
-            }
-        }while(addMore);
+					if (!added){//if not added
+						addMore=false;// stop to loop by making addMore = false because mas occupancy has been reached
+					}
+					addMore = true;// else addMore = true so keep going
+					
+				}
+				else{
+					addMore=false;// if users enteres anything other than "y" stop the loop
+				}
+			}while(addMore);
         c1.organize();//organize the tables
         System.out.println(c1);//print tables
     }
